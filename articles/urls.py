c871d2df62d22f8ec7ca8a8d09f983ae704fb367
Robin Berghuijs <robin@robinberghuijs.com>
2016-08-17 10:56:16 +0200

    make logging in required for creating, updating, and deleting articles

--- articles/urls.py
+++ articles/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from .views import *
 
 urlpatterns = [
@@ -8,8 +9,8 @@ urlpatterns = [
     url(r'^category/(?P<pk>\d+)/$', CategoryView.as_view(), name='category'),
     url(r'^cities/$', CitiesView.as_view(), name='cities'),
     url(r'^city/(?P<pk>\d+)/$', CityView.as_view(), name='city'),
-    url(r'^article-submit/$', ArticleSubmitView.as_view(), name='article-submit'),
-    url(r'^article-update/(?P<pk>\d+)/$', ArticleUpdateView.as_view(), name='article-update'),
-    url(r'^article-delete/(?P<pk>\d+)/$', ArticleDeleteView.as_view(), name='article-delete'),
+    url(r'^article-submit/$', login_required(ArticleSubmitView.as_view()), name='article-submit'),
+    url(r'^article-update/(?P<pk>\d+)/$', login_required(ArticleUpdateView.as_view()), name='article-update'),
+    url(r'^article-delete/(?P<pk>\d+)/$', login_required(ArticleDeleteView.as_view()), name='article-delete'),
     url(r'^profile/(?P<pk>\d+)/$', ProfileView.as_view(), name='profile'),
 ]
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -2,6 +2,18 @@ from .base import FunctionalTest
 
 class LoginTest(FunctionalTest):
 
+    def test_cannot_submit_article_without_logging_in(self):
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_title').send_keys('Title')
+        self.browser.find_element_by_id('id_text').send_keys('Text')
+        self.browser.find_element_by_id('id_categories_1').click()
+        self.browser.find_element_by_id('id_city_1').click()
+        self.browser.find_element_by_css_selector('.article-form input.button').click()
+
+        container = self.browser.find_element_by_class_name('allauth-container')
+
+        self.assertIn("Log in", container)
+
     def test_login_with_email(self):
         # Person goes to Togethere and sees login link
         self.browser.get(self.server_url)
