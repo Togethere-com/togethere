474e654f490db891c244f7c3404889542ea486d6
Robin Berghuijs <robin@robinberghuijs.com>
2016-09-08 09:14:18 +0200

    remove required attribute on hidden textfield when tinymce loads (for chrome)

--- articles/static/articles/src/scripts/main.js
+++ articles/static/articles/src/scripts/main.js
@@ -6,7 +6,23 @@ function DOMContentLoaded() {
     var $e = $('.tinymce');
     // .tinymce element is not present on all pages so check for it
     if ($e.length != 0) {
-      var tinymceConf = $e.data('mce-conf');
+      if ($e.data('mce-conf').length != 0) {
+        var tinymceConf = $e.data('mce-conf');
+      } else {
+        var tinymceConf = {
+          theme: "modern",
+          toolbar: "undo redo | bold italic | bullist numlist | blockquote | removeformat",
+          menubar: False,
+          statusbar: False,
+          schema: "html5",
+          max_height: 500,
+          max_width: 500,
+          min_height: 100,
+          min_width: 400,
+          content_css: '/static/articles/build/css/tinymce.css',
+        }
+      }
+      document.getElementById('id_text').removeAttribute("required");
       tinymce.remove();
       tinymce.init(tinymceConf);
     }
