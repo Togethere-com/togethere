95b6e8ef4834fc57f23d174e8dd0bdf8396ade7f
Robin Berghuijs <robin@robinberghuijs.com>
2016-12-30 11:58:15 +0100

    set CSP for typekit

--- togethere/settings.py
+++ togethere/settings.py
@@ -174,3 +174,10 @@ HAYSTACK_CONNECTIONS = {
         'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
     },
 }
+
+# Django-csp configuration
+CSP_CONNECT_SRC = ("'self'","performance.typekit.net",)
+CSP_SCRIPT_SRC = ("'self'","use.typekit.net",)
+CSP_STYLE_SRC = ("'self'","'unsafe-inline'","use.typekit.net",)
+CSP_FONT_SRC = ("'self'","use.typekit.net","fonts.typekit.net",)
+CSP_IMG_SRC = ("'self'","p.typekit.net",)
